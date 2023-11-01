aa460bda351e5b2cddb4c6322a4bfee79b254744
nakamurakazuki <nakamura.kazuki@algoage.co.jp>
2023-11-01 19:03:52 +0900

    システムテストを作成した
    Capybaraを使用してシステムスペックを作成した

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,47 +1,80 @@
-require 'rails_helper'
+class TasksController < ApplicationController
+  before_action :set_project
+  before_action :project_owner?
+  before_action :set_task, only: %i[ show edit update destroy toggle ]
 
-RSpec.describe TasksController, type: :controller do
-  before do
-    @user = FactoryBot.create(:user)
-    @project = FactoryBot.create(:project, owner: @user)
-    @task = @project.tasks.create!(name: "Test task")
+  # TODO: This action is not used.
+  # GET /tasks or /tasks.json
+  def index
+    @tasks = Task.all
   end
 
-  describe "#show" do
-    it "responds with JSON formatted output" do
-      sign_in @user
-      get :show, format: :json,
-        params: { project_id: @project.id, id: @task.id }
-      expect(response.content_type).to include "application/json"
+  # TODO: This action is not used.
+  # GET /tasks/1 or /tasks/1.json
+  def show
+  end
+
+  # GET /tasks/new
+  def new
+    @task = @project.tasks.new
+  end
+
+  # GET /tasks/1/edit
+  def edit
+  end
+
+  # POST /tasks or /tasks.json
+  def create
+    @task = @project.tasks.new(task_params)
+
+    respond_to do |format|
+      if @task.save
+        format.html { redirect_to @project, notice: "Task was successfully created." }
+        format.json { render :show, status: :created, location: [@task.project, @task] }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+      end
     end
   end
 
-  describe "#create" do
-    it "responds with JSON formatted output" do
-      new_task = { name: "New test task" }
-      sign_in @user
-      post :create, format: :json,
-        params: { project_id: @project.id, task: new_task }
-      expect(response.content_type).to include "application/json"
-    end
-
-    it "adds a new task to the project" do
-      new_task = { name: "New test task" }
-      sign_in @user
-      expect {
-        post :create, format: :json,
-          params: { project_id: @project.id, task: new_task }
-      }.to change(@project.tasks, :count).by(1)
-    end
-
-    it "requires authentication" do
-      new_task = { name: "New test task" }
-      # Don't sign in this time ...
-      expect {
-        post :create, format: :json,
-          params: { project_id: @project.id, task: new_task }
-      }.to_not change(@project.tasks, :count)
-      expect(response).to_not be_successful
+  # PATCH/PUT /tasks/1 or /tasks/1.json
+  def update
+    respond_to do |format|
+      if @task.update(task_params)
+        format.html { redirect_to @project, notice: "Task was successfully updated." }
+        format.json { render :show, status: :ok, location: @task }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+      end
     end
   end
+
+  # DELETE /tasks/1 or /tasks/1.json
+  def destroy
+    @task.destroy
+
+    respond_to do |format|
+      format.html { redirect_to @project, notice: "Task was successfully destroyed.", status: :see_other }
+      format.json { head :no_content }
+    end
+  end
+
+  def toggle
+    @task.update(completed: !@task.completed)
+
+    render turbo_stream: turbo_stream.replace(@task, partial: 'completed', locals: { task: @task })
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_task
+      @task = Task.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def task_params
+      params.require(:task).permit(:name, :project_id, :completed)
+    end
 end
