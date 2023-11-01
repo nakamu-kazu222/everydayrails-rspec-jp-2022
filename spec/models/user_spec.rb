2251a17bdb00e01c835dd7f5f31b413dc1b50bb1
nakamurakazuki <nakamura.kazuki@algoage.co.jp>
2023-11-01 11:08:03 +0900

    ファクトリを使用してユーザーの有効性をテストした

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  # 有効なファクトリを持つこと
+  it "has a valid factory" do
+    expect(FactoryBot.build(:user)).to be_valid
+  end
 
   # 姓、名、メール、パスワードがあれば有効な状態であること
   it "is valid with a first name, last name, email, and password" do
