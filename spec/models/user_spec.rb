4dcc96aef9d1a313be763dcad0298d8639b7892a
nakamurakazuki <nakamura.kazuki@algoage.co.jp>
2023-11-02 01:03:28 +0900

    Shoulda Matchers を使用して簡略化した

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,33 +18,16 @@ RSpec.describe User, type: :model do
   end
 
   # 名がなければ無効な状態であること
-  it "is invalid without a first name" do
-    user = FactoryBot.build(:user, first_name: nil)
-    user.valid?
-    expect(user.errors[:first_name]).to include("can't be blank")
-  end
+  it { is_expected.to validate_presence_of :first_name }
 
   # 姓がなければ無効な状態であること
-  it "is invalid without a last name" do
-    user = FactoryBot.build(:user, last_name: nil)
-    user.valid?
-    expect(user.errors[:last_name]).to include("can't be blank")
-  end
+  it { is_expected.to validate_presence_of :last_name }
 
   # メールアドレスがなければ無効な状態であること
-  it "is invalid without an email address" do
-    user = FactoryBot.build(:user, email: nil)
-    user.valid?
-    expect(user.errors[:email]).to include("can't be blank")
-  end
+  it { is_expected.to validate_presence_of :email }
 
   # 重複したメールアドレスなら無効な状態であること
-  it "is invalid with a duplicate email address" do
-    FactoryBot.create(:user, email: "aaron@example.com")
-    user = FactoryBot.build(:user, email: "aaron@example.com")
-    user.valid?
-    expect(user.errors[:email]).to include("has already been taken")
-  end
+  it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
 
     # ユーザーのフルネームを文字列として返すこと
   it "returns a user's full name as a string" do
@@ -52,13 +35,5 @@ RSpec.describe User, type: :model do
     expect(user.name).to eq "John Doe"
   end
 
-  it "returns a user's full name as a string" do
-    user = User.new(
-    first_name: "John",
-    last_name:  "Doe",
-    email:      "johndoe@example.com",
-    )
-    expect(user.name).to eq "John Doe"
-  end
 
 end
